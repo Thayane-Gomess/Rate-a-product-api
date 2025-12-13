9c36007cf746d81871f3ab204403e31e9d89ba0a
Thayane Gomes <thayaneleandrooo@gmail.com>
2025-12-13 19:26:40 -0300

    Resolvido

--- src/server.js
+++ src/server.js
@@ -1,30 +1,16 @@
-<<<<<<< HEAD
-/* eslint-disable */
-require("dotenv").config();
-const app = require("./app");
-const sequelize = require("./database");
-
-require("./models/Product");
-
-const PORT = process.env.PORT || 3000;
-
-sequelize
-  .sync({ alter: true })
-  .then(() => {
-    console.log("Banco sincronizado");
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando na porta ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Erro ao conectar no banco:", err);
-  });
-=======
 import app from "./app.js";
+import sequelize from "./config/database.js";
+
+// Importa os models para sincronizar com o banco
+import "./models/user.js";
+import "./models/product.js";
+import "./models/review.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+sequelize.sync().then(() => {
+  console.log("Banco conectado e sincronizado!");
+  app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
+  });
 });
->>>>>>> b326ce3e7ca3250bc60f6568ae93910947983c93
